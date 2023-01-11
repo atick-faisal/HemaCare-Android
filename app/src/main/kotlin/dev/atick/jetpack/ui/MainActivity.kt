cee45a758f665f4ea6f84bfe084dc2ad5b7e08d3
Atick Faisal <atickfaisal@gmail.com>
2023-01-11 09:54:09 +0300

    Implement ID screen

--- app/src/main/kotlin/dev/atick/jetpack/ui/MainActivity.kt
+++ app/src/main/kotlin/dev/atick/jetpack/ui/MainActivity.kt
@@ -3,12 +3,10 @@ package dev.atick.jetpack.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
+import dev.atick.jetpack.ui.id.NavGraphs
 import dev.atick.jetpack.ui.theme.JetpackTheme
 
 @AndroidEntryPoint
@@ -21,18 +19,5 @@ class MainActivity : ComponentActivity() {
                 DestinationsNavHost(navGraph = NavGraphs.root)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/kotlin/dev/atick/jetpack/ui/id/IdScreen.kt
@@ -0,0 +1,66 @@
+package dev.atick.jetpack.ui.id
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import dev.atick.jetpack.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Destination
+@RootNavGraph(start = true)
+fun IdScreen(
+    navigator: DestinationsNavigator
+) {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.surface)
+    ) {
+        Column(Modifier.padding(32.dp)) {
+            Text(
+                text = "Give the Patient an Unique ID",
+                fontWeight = FontWeight.Thin,
+                fontSize = 56.sp,
+                lineHeight = 64.sp,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            TextField(
+                value = "",
+                onValueChange = {},
+                placeholder = {
+                    Text(text = "For Example: 001")
+                }
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Next")
+            }
+        }
+
+        Image(
+            modifier = Modifier
+                .width(240.dp)
+                .height(360.dp)
+                .align(Alignment.BottomEnd),
+            painter = painterResource(id = R.drawable.plant),
+            contentDescription = "plant"
+        )
+    }
+}
